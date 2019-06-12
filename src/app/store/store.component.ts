f74566e7b5278022e56da94f0e8c9ff622c07c04
Alex <alex@Alexs-iMac.local>
2019-06-12 15:03:59 +0300

    add cart to shop

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../model/product.model';
 import { ProductRepository } from '../model/product.repository';
+import { Cart } from "../model/cart.model";
 
 @Component({
   selector: 'app-store',
@@ -13,7 +14,8 @@ export class StoreComponent implements OnInit {
   public productsPerPage = 4;
   public selectedPage = 1;
 
-  constructor(private repository: ProductRepository) { }
+  constructor(private repository: ProductRepository,
+    private cart: Cart) { }
 
   get products(): Product[] {
     let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
@@ -40,7 +42,11 @@ export class StoreComponent implements OnInit {
 
   get pageCount(): number {
     return Math.ceil(this.repository
-        .getProducts(this.selectedCategory).length / this.productsPerPage)
+      .getProducts(this.selectedCategory).length / this.productsPerPage)
+  }
+
+  addProductToCart(product: Product) {
+    this.cart.addLine(product);
   }
 
   ngOnInit() {
